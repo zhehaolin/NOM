c3f943a320d2da00a1549076d3b795a9d12198ae
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-11-03 11:31:59 -0400

    a

---
+++ NightOfTheMuseum/src/victorRem/VictorRoom.java
@@ -0,0 +1,12 @@
+package victorRem;
+
+import caveExplorer.CaveRoom;
+
+public class VictorRoom extends CaveRoom{
+
+	public VictorRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
