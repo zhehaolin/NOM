b0534a4a08fa931127d0edcc01f52856c02eae1c
JasonYan <jyan7555@bths.edu>
2017-11-10 23:39:15 -0500

    Updated Inventory

--- NightOfTheMuseum/src/jasonDavid/JasonNPC.java
+++ NightOfTheMuseum/src/jasonDavid/JasonNPC.java
@@ -5,9 +5,19 @@ import caveExplorer.Inventory;
 
 
 public class JasonNPC extends NPC{
-	
+	private static final String[] responses = {"Get robbed!", "I'm feeling generous"};
+	 
 	public void interact() {
-		CaveExplorer.print("Haha!");
-		Inventory.changeGold(-500);
+		int ranNum = JasonRoom.randomNum(0, 1);
+		chooseMove(ranNum);
+		CaveExplorer.print(responses[ranNum]);
+	}
+	public void chooseMove(int num) {
+		if (num == 0) {
+			Inventory.changeGold(-500);
+		}
+		else if (num==1) {
+			Inventory.changeGold(500);
+		}
 	}
 }
--- NightOfTheMuseum/src/jasonDavid/JasonRoom.java
+++ NightOfTheMuseum/src/jasonDavid/JasonRoom.java
@@ -6,7 +6,7 @@ import caveExplorer.NPCRoom;
 
 public class JasonRoom extends NPCRoom{
 	
-	public JasonNPC jason;
+	public static JasonNPC jason = new JasonNPC();
 	
 	public JasonRoom(String description) {
 		super(description);
@@ -20,23 +20,27 @@ public class JasonRoom extends NPCRoom{
 	}
 	public void performAction(int direction) {
 		if (direction == 4) {
-			CaveExplorer.currentRoom.leave();
-			CaveExplorer.print("You have been teleported!)");
-			CaveExplorer.currentRoom = CaveExplorer.caves[randomNum(0,9)][randomNum(0,9)];
-			CaveExplorer.currentRoom.enter();
+			teleport(randomNum(0,9),randomNum(0,9));
 		}else {
-			if (direction == 5){
-				if(containsNPC() && jason.isActive()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       ) {
-					System.out.println("asd");
+			if (direction == 5){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+				{
+					CaveExplorer.print("hi");
 					jason.interact();
 				}	
 			}else {
-				System.out.println("That key does nothing");
+				CaveExplorer.print(":)");
+				super.performAction(direction);
 			}
 		}
 	}
-	public int randomNum(int min, int max) {
-		return (int)(Math.random() *(max-min)) + min;
+	public static int randomNum(int min, int max) {
+		return (int)(Math.random() *(max-min+1)) + min;
+	}
+	public void teleport(int num1, int num2) {
+		CaveExplorer.print("You have been teleported!)");
+		CaveExplorer.currentRoom.leave();
+		CaveExplorer.currentRoom = CaveExplorer.caves[num1][num2];
+		CaveExplorer.currentRoom.enter();
 	}
 	
 }
