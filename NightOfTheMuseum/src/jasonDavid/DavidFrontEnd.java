5c82eb25ee9c689f2b2c65780c90de8e3667f987
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-11-14 11:37:02 -0500

    omae wa mou shindeiru

--- NightOfTheMuseum/src/jasonDavid/DavidFrontEnd.java
+++ NightOfTheMuseum/src/jasonDavid/DavidFrontEnd.java
@@ -1,12 +1,23 @@
 package jasonDavid;
+import java.util.Scanner;
 
 public class DavidFrontEnd implements JasonSupport{
 	private DavidSupport backend;
+	public static Scanner sc = new Scanner(System.in);
+	public static boolean win = false;
 	
-	public DavidFrontEnd() {
-		backend = new JasonBackEnd(this);
+	public DavidFrontEnd(int size) {
+		backend = new JasonBackEnd(this, size);
 	}
 	public static void main(String[] arg) {
-		DavidFrontEnd demo = new DavidFrontEnd();
+		System.out.println("What size?");
+		int size = sc.nextInt();
+		DavidFrontEnd demo = new DavidFrontEnd(size);
+		demo.play();
+	}
+	private void play() {	
+		while(!win) {
+			//while the player didnt win yet
+		}
 	}
 }
--- NightOfTheMuseum/src/jasonDavid/JasonBackEnd.java
+++ NightOfTheMuseum/src/jasonDavid/JasonBackEnd.java
@@ -2,10 +2,31 @@ package jasonDavid;
 
 public class JasonBackEnd implements DavidSupport{
 	private JasonSupport frontend;
-	
-	public JasonBackEnd(JasonSupport frontEnd) {
-		frontend = frontEnd;
-	}
+	private int[][] table;
+	private int[] chosen;
 
+	public JasonBackEnd(DavidFrontEnd frontEnd, int size) {
+		frontend = frontEnd;
+		createTable(size);
+	}
+	/**
+	 * creates a size x size table using double for loop
+	 * @param size
+	 */
+	public void createTable(int size) {
+		for (int i=0; i<size; i++) {
+			for ( int j=0; j<size; j++) {
+				table[i][j] = generateUniqueRandomNumber();
+			}
+		}
+	}
+	/**
+	 * generate unique random number that is not chosen yet
+	 * @return
+	 */
+	private int generateUniqueRandomNumber() {
+		return 0;
+	}
+	
 }
 
