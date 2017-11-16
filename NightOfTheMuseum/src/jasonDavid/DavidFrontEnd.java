cd6cd9185fc6b3e69bdc2446237314568372b2ab
zhoud <zhoud@DESKTOP-RLUV3JL>
2017-11-16 15:39:07 -0500

    down together

--- NightOfTheMuseum/src/jasonDavid/DavidFrontEnd.java
+++ NightOfTheMuseum/src/jasonDavid/DavidFrontEnd.java
@@ -15,21 +15,24 @@ public class DavidFrontEnd implements JasonSupport{
 
 	public static void main(String[] arg) {
 		System.out.println("What size?");
-		int size = CaveExplorer.in.nextInt();
+		//int size = CaveExplorer.in.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
 		DavidFrontEnd demo = new DavidFrontEnd(size);
 		demo.play();
 	}
 	public void play() {	
-		while(backend.stillPlaying()) {
-			 displayBoard();
-			 String input = getValidUserInput();
-		     if(!backend.respondToInput(input)) {
-		    	 provideHint();
-		     }
-			
-		      backend.analyzeBoard();
-		    }
-		    printGameOverMessage();
+		displayBoard();
+		//while(backend.stillPlaying()) {
+			// displayBoard();}
+			// String input = getValidUserInput();
+		    // if(!backend.respondToInput(input)) {
+		    //	 provideHint();
+		    // }
+			//
+		     // backend.analyzeBoard();
+		   // }
+		   // printGameOverMessage();
 	}
 
 	private void provideHint() {
@@ -47,41 +50,38 @@ public class DavidFrontEnd implements JasonSupport{
 	private void displayBoard() {
 		map = " ";
 		//create line across top:
-		for(int i = 0; i < CaveExplorer.caves[0].length -1; i++) {
+		for(int i = 0; i < backend.table[0].length -1; i++) {
 			map += "____";//4 underscores
 		}
 		map+= "___\n";//3 underscores, makes the corner look symmetrical
-		for(CaveRoom[] row : CaveExplorer.caves) {
+		for(int[] row : backend.table) {
 			//3 rows of text
 			for(int i = 0; i < 3; i++) {
 				String text = "";
-				for(CaveRoom cr : row) {
-					//if door is open, leave open
-					if(cr.getDoor(CaveRoom.WEST) != null &&
-							cr.getDoor(CaveRoom.WEST).isOpen()) {
-						text += " ";
-					}else {
+				for(int cr : row) {
 						text += "|";
-					}
 					//contents of room depend on what row this is
 					if(i==0) {
 						text+="   ";//3 spaces
 					}else if(i == 1) {
-						text += " "+cr.getContents()+" ";
-					}else if(i == 2) {
-						//draw space if door to south is open
-						if(cr.getDoor(CaveRoom.SOUTH) != null && 
-						cr.getDoor(CaveRoom.SOUTH).isOpen()){
-							text+="   ";//3 spaces
-						}else {
-							text += "___";
+						if (0 < cr && cr< 10){
+						text += " "+cr+" ";
 						}
+						if (9<cr && cr< 100){
+						text += ""+cr+" ";
+						}
+						if (99<cr&&cr<1000){
+							text+=cr;
+						}
+					}else if(i == 2) {
+						text += "___";
 					}
 				}//last caveroom in row
 				text+="|";
 				map += text +"\n";
 			}
 		}
+		System.out.print(map);
 	}
 
 }
