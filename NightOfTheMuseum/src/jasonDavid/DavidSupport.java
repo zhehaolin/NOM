3744152e53699aa5af6a39eac996ed82ca520284
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-11-17 11:35:30 -0500

    Finished back end

--- NightOfTheMuseum/src/jasonDavid/DavidSupport.java
+++ NightOfTheMuseum/src/jasonDavid/DavidSupport.java
@@ -4,7 +4,7 @@ public interface DavidSupport {
 
 	boolean stillPlaying();
 
-	boolean respondToInput(String input);
+	boolean respondToInput(String input1, String input2);
 
 	void analyzeBoard();
 
