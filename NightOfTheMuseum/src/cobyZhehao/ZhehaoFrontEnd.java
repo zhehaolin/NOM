a5b1b07fbb7b53d8fba68f362b48b3745b698477
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-11-21 11:28:17 -0500

    z

--- NightOfTheMuseum/src/cobyZhehao/ZhehaoFrontEnd.java
+++ NightOfTheMuseum/src/cobyZhehao/ZhehaoFrontEnd.java
@@ -29,7 +29,7 @@ public class ZhehaoFrontEnd implements CobySupport {
 		String command = ZhehaoUtility.waitForLetterInput("rp");
 		if(command.equals("r")){
 			ZhehaoUtility.print("You have to solve the puzzle to unlock the door. In a 4 by 4 square ,you have to place the numbers in order from 1 to 15. Enter the coord of the number "
-					+ "you want to move.");
+					+ "you want to move."+"\n press enter to continue");
 			in.nextLine();
 			menu();
 		}else{
@@ -51,9 +51,7 @@ public class ZhehaoFrontEnd implements CobySupport {
 			 updateMap();
 			 displaymovestaken(p);
 			 System.out.println("Which tile do you want to move? ");
-			
 				 displayhints(p);
-			
 				 int[] coords = backend.getCoordInput();
 				 if(coords !=null) {
 					 p = plots[coords[0]][coords[1]];
@@ -72,13 +70,13 @@ public class ZhehaoFrontEnd implements CobySupport {
 	}
 	private void displayhints(ZhehaoCobyPlot p) {
 		ZhehaoCobyPlot[][] plot=backend.getPlots();
-		String[] possiblehint= {"-Locate tiles 1 and 2.\n -Move the tile so that the blank space is in the next position you want to move the tile to.\n -Moves the tile into the blank space.\n -Repeat this process utile tiels 1 and 2 are in the correct positions."
+		String[] possiblehint= {" -Locate tiles 1 and 2.\n -Move the tile so that the blank space is in the next position you want to move the tile to.\n -Moves the tile into the blank space.\n -Repeat this process utile tiels 1 and 2 are in the correct positions."
 		,"-Move tile 4 into tiles 3's final position.\n - Move tiles 3 to be directly under tile.","-Move the blank square to the top right corner.\n -Move titles three and four like they are a snake so that they are both in their final positions.",
 		"-Locate tiles 5 and 6.\n -Move tiles 5 and 6 to thier final positions in a similar way that you moves tiles 1 and 2 into their final positions.","-Move tile 8 into the final position of tile 7.\n-Move tile 7 to be directly under tile 8.","-Move the blank space to be on the right hand side of tile 8.\n -Move tiles 8 and 7 like a snake into their final positions.",
 		"-Move tile 13 to be in tile 9's final position.\n -Move tile 9 to be directly to the right of tile 13.","-Rotate the 5 tiles in the bottom left of the board counter clockwise like a snake until tiles 13 and 9 are in their final positions.","-Move tile 14 to the right of tile 9 so that it is in tile 10's final position.\n -Move tile 10 to be directly to the right of tile 14.",
 		"-Rotate the three tiles in the middle two columns on the bottom two rows counter clockwise like a snake until tiles 14 and 10 are in their final positions.","-Rotate tiles 11, 12, and 15 either clockwise or counter clockwise until they are all in the correct positions"};
 		
-		System.out.println(possiblehint[checkplot(plot)]);
+		System.out.println("Hints:\n"+possiblehint[checkplot(plot)]);
 		
 	}
 	private int checkplot(ZhehaoCobyPlot[][] z) {
@@ -104,7 +102,7 @@ public class ZhehaoFrontEnd implements CobySupport {
 		for(int i = 0; i < backend.plots[0].length -1; i++) {
 			map += "____";
 		}
-		map+= "___\n";
+		map+= "____\n";
 		for(ZhehaoCobyPlot[] row : backend.plots) {
 			
 			for(int i = 0; i < 3; i++) {
@@ -116,7 +114,12 @@ public class ZhehaoFrontEnd implements CobySupport {
 					if(i==0) {
 						text+="   ";
 					}else if(i == 1) {
-						text += " "+cr.getContents()+" ";
+						if(cr.getContents().length()==1) {
+							text += " "+cr.getContents()+" ";
+						}else {
+							text+=" "+cr.getContents();
+						}
+						
 					}else if(i == 2) {
 							text += "___";
 					}
