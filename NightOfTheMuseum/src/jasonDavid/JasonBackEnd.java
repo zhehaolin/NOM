8cb47ca7f9d4529f119aee1e03478f6258fd34a7
jyan7555 <jyan7555@bths.edu>
2017-11-22 11:40:39 -0500

    Update JasonBackEnd.java

--- NightOfTheMuseum/src/jasonDavid/JasonBackEnd.java
+++ NightOfTheMuseum/src/jasonDavid/JasonBackEnd.java
@@ -1,173 +1,174 @@
 package jasonDavid;
+import java.util.Scanner;
+import caveExplorer.CaveExplorer;
+import caveExplorer.CaveRoom;
 
-public class JasonBackEnd implements DavidSupport{
-	public JasonSupport frontend;
-	public static int[][] table;
-	public boolean playing;
+public class DavidFrontEnd implements JasonSupport{
 	
-	public static int[] chosen;
-	public int magicNumber;
+	private JasonBackEnd backend;
+	private String map;
+
 	
-	public JasonBackEnd(DavidFrontEnd frontEnd, int size) {
-		table = new int[size][size];
-		chosen = new int[size*size];
-		frontend = frontEnd;
-		createTable(size);
-		magicNumber = size* ((int)Math.pow(size, 2)+1)/2;
-		playing = true;
+	public DavidFrontEnd(int size) {
+		backend = new JasonBackEnd(this, size);
 	}
-	/**
-	 * creates a size x size table using double for loop
-	 * @param size
-	 */
-	public void createTable(int size) {
-		for (int i=0; i<size; i++) {
-			for ( int j=0; j<size; j ++) {
-				table[i][j] = generateUniqueRandomNumber(size);
-			}
-		}
+
+	public static void main(String[] arg) {
+		System.out.println("What size?");
+		//int size = CaveExplorer.in.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		DavidFrontEnd demo = new DavidFrontEnd(size);
+		demo.play();
 	}
-	/**
-	 * generate unique random number that is not chosen yet
-	 * @return
-	 */
-	public int generateUniqueRandomNumber(int size) {
-		int num = (int)(Math.random()*size*size)+1;
-		while(isRepeated(num)) {
-			num = (int)(Math.random()*size*size)+1;
-		}
-		return num;
+	public void play() {	
+		while(backend.stillPlaying()) {
+			 displayBoard();
+			 Scanner sc = new Scanner(System.in);
+			 String input1 = sc.nextLine();
+			 String input2 = sc.nextLine();
+		     if(!backend.respondToInput(input1,input2)) {
+		    	 provideHint();
+		    	 System.out.println("hi");
+		     }else {
+		    	backend.respondToInput(input1,input2); 
+		    	backend.performSwap(input1, input2,backend.commaPos(input1),backend.commaPos(input2));
+		    	//System.out.println(backend.commaPos(input1));
+		     }
+		      //backend.analyzeBoard();
+		    }
+		  // printGameOverMessage();
+	
+	}
+
+	private void provideHint() {
 		
 	}
-	public boolean isRepeated(int num) {
-		for (int i=0; i<chosen.length; i++) {
-			if (chosen[i] == num) {
-				return true;
-			}
-		}
-		addNum(num);
-		return false;
+
+	private void printGameOverMessage() {
+		System.out.println("Click! A sound ringed loudly signifying the completion of the puzzle.");
 	}
-	private void addNum(int num) {
-		for (int i=0; i<chosen.length;i++) {
-			if (chosen[i] == 0) {
-				chosen[i] = num;
-				return;
-			}
-		}
+
+	private String getValidUserInput() {
+		 return CaveExplorer.in.nextLine();
 	}
-	/**
-	 * swaps position of the numbers
-	 */
-	public void swap(int[][] box, int x1, int y1, int x2, int y2){
-		int holder = box[x1][y1];
-		box[x1][y1] = box[x2][y2];	
-		box[x2][y2] = holder;
+
+	private void displayBoard() {
+		map = " ";
+		//create line across top:
+		for(int i = 0; i < backend.table[0].length -1; i++) {
+			map += "____";//4 underscores
+		}
+		map+= "___\n";//3 underscores, makes the corner look symmetrical
+		for(int[] row : backend.table) {
+			//3 rows of text
+			for(int i = 0; i < 3; i++) {
+				String text = "";
+				for(int cr : row) {
+					text += "|";
+					//contents of room depend on what row this is
+					if(i==0) {
+						text+="   ";//3 spaces
+					}else if(i == 1) {
+						if (0 < cr && cr< 10){
+						text += " "+cr+" ";
+						}
+						if (9<cr && cr< 100){
+						text += ""+cr+" ";
+						}
+						if (99<cr&&cr<1000){
+							text+=cr;
+						}
+					}else if(i == 2) {
+						text += "___";
+					}
+				}//last caveroom in row
+				text+="|";
+				map += text +"\n";
+			}
+		}
+		System.out.print(map);
+		System.out.print(SumOfArrays(backend.table));
 	}
-		
-	/**
-	 * Formula: M=n(n^2+1)/2 M= winning number n= width of table
-	 * Checks to see if winning condition is met
-	 * @return
-	 */
-	public boolean calculateWin() {
-		for(int i=0; i<table.length;i++) {
-			if(doubleForLoopCalc(i,"row") != magicNumber) {
-				return false;
+
+	private String SumOfArrays(int[][] table) {
+		String rows="";
+		String columns="";
+		int rowcount = 1;
+		int columncount = 1;
+		for (int[] row : table){
+			int valuer = 0;
+			for (int cr : row){
+				valuer += cr;
 			}
+		rows += "Row "+rowcount+" sum:"+valuer+"\n";
+		rowcount+=1;
 		}
-		for (int j=0; j<table[0].length; j++) {
-			if (doubleForLoopCalc(j,"column") != magicNumber) {
-				return false;
+		for (int i=0; i<table.length; i++) {
+			int valuec = 0;
+			for (int[] row : table) {
+				valuec += row[i];
 			}
+			columns+= "Columns "+columncount+" sum:"+valuec+"\n";
+			columncount++;
 		}
-		if (calcDiagonal("left") != magicNumber || calcDiagonal("right") != magicNumber) {
-			System.out.println(calcDiagonal("right"));
-			return false;
+		int valuetl = 0;
+		for (int i=0; i<table.length;i++) {
+			valuetl += table[i][i];
 		}
-		return true;
+		String tl = "Top left to bottom right sum:"+valuetl;
+		int valuetr = 0;
+		int j = table.length-1;
+		for (int i=0; i<table.length;i++) {
+				valuetr += table[i][j];
+				j-=1;
+		}
+		String tr = "Top right to bottom left sum:"+valuetr;
+		String finito = rows+"\n"+columns+"\n"+tl+"\n"+tr;
+		return finito;
 	}
-	/**
-	 * Use a double for loop to check the sum of individual column/row
-	 * @param num
-	 * @param start
-	 * @return
-	 */
-	public int doubleForLoopCalc(int num, String start) {
-		int count =0;
-		if (start.equals("row")) {
-			for (int i=0; i < table[num].length;i++) {
-				count += table[num][i];
+	private boolean checkForNext(int xcoord, int ycoord) {
+		int[][] table = backend.table;
+		if (table[ycoord-1][xcoord] < 0) {
+			if (table[table.length-1][xcoord+1] < 0){
+				if (table[table.length-1][0] == table[ycoord][xcoord]+1) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				if (table[table.length-1][xcoord+1]==table[ycoord][xcoord]+1) {
+					return true;
+				}
+				else {
+					return false;
+				}
 			}
 		}
-		else {
-			for (int i=0; i<table.length; i++) {
-				count+= table[i][num];
+		if (table[ycoord][xcoord+1]<0) {
+			if (table[ycoord-1][0]<0) {
+				if(table[table.length-1][0]==table[ycoord][xcoord]+1) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				if (table[ycoord-1][0] == table[ycoord][xcoord]+1) {
+					return true;
+				}
+				else {
+					return false;
+				}
 			}
 		}
-		return count;
-	}
-	public int calcDiagonal(String start) {
-		int count =0;
-		if (start.equals("left")) {
-			int row =0;
-			int col =0;
-			while (row < table.length && col < table[0].length) {
-				count+= table[row][col];
-				row++;
-				col++;
-			}
-		}
-		else {
-			int x= 0;
-			int y= table.length-1;
-			while (x < table.length && y >= 0) {
-				count+= table[x][y];
-				x++;
-				y--;
-			}
-		}
-		return count;
-	}
-	public boolean stillPlaying() {
-		return playing;
-	}
-	public boolean respondToInput(String input1, String input2) {
-		if (isValid(input1) && isValid(input2)) {
-			
-			
+		if (table[ycoord][xcoord] == 1+table[ycoord-1][xcoord+1]) {
 			return true;
 		}
-		return false;
+			return false;
 	}
-	public void performSwap(String input1, String input2) {
-		int y = Integer.parseInt(input1.substring(0,1))-1;
-		int x = Integer.parseInt(input1.substring(2,3))-1;
-		int y1 = Integer.parseInt(input2.substring(0,1))-1;
-		int x1 = Integer.parseInt(input2.substring(2,3))-1;
-		swap(table,x,y,x1,y1);
-	}
-	public void analyzeBoard() {
-		playing = calculateWin();
-	}
-	public boolean isValid(String input) {
-		return input.length()==3 &&isNum(input.substring(0,1)) && isNum(input.substring(2,3)) && input.substring(1,2).equals(",");
-	}
-	public static boolean isNum(String input) {
-	    boolean isNum = true;
-	    try {
-
-	        Double.parseDouble(input);
-
-	    }catch (NumberFormatException e) {
-	        isNum = false;
-	    }
-	    return isNum;
-	}
-	public boolean isCheatCode(String str) {
-		return str.equals("getgood");
-	}
-	//s
 }
-
 
