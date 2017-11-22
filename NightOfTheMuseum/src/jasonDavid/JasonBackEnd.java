4ddabbda398d0f8fda08fed3f074c739ffbd70f6
jyan7555 <jyan7555@bths.edu>
2017-11-22 11:42:45 -0500

    Update JasonBackEnd.java

--- NightOfTheMuseum/src/jasonDavid/JasonBackEnd.java
+++ NightOfTheMuseum/src/jasonDavid/JasonBackEnd.java
@@ -4,7 +4,7 @@ public class JasonBackEnd implements DavidSupport{
 	public JasonSupport frontend;
 	public static int[][] table;
 	public boolean playing;
-	
+	public int sizeLength;
 	public static int[] chosen;
 	public int magicNumber;
 	
@@ -15,6 +15,7 @@ public class JasonBackEnd implements DavidSupport{
 		createTable(size);
 		magicNumber = size* ((int)Math.pow(size, 2)+1)/2;
 		playing = true;
+		sizeLength = Integer.toString(size).length();
 	}
 	/**
 	 * creates a size x size table using double for loop
@@ -134,26 +135,47 @@ public class JasonBackEnd implements DavidSupport{
 	}
 	public boolean respondToInput(String input1, String input2) {
 		if (isValid(input1) && isValid(input2)) {
-			
-			
 			return true;
 		}
 		return false;
 	}
-	public void performSwap(String input1, String input2) {
-		int y = Integer.parseInt(input1.substring(0,1))-1;
-		int x = Integer.parseInt(input1.substring(2,3))-1;
-		int y1 = Integer.parseInt(input2.substring(0,1))-1;
-		int x1 = Integer.parseInt(input2.substring(2,3))-1;
+	public void performSwap(String input1, String input2, int psn1, int psn2) {
+		int y = Integer.parseInt(input1.substring(0,psn1))-1;
+		int x = Integer.parseInt(input1.substring(psn1+1,input1.length()))-1;
+		int y1 = Integer.parseInt(input2.substring(0,psn2))-1;
+		int x1 = Integer.parseInt(input2.substring(psn2+1,input2.length()))-1;
 		swap(table,x,y,x1,y1);
+		System.out.println(psn1);
 	}
 	public void analyzeBoard() {
 		playing = calculateWin();
 	}
 	public boolean isValid(String input) {
-		return input.length()==3 &&isNum(input.substring(0,1)) && isNum(input.substring(2,3)) && input.substring(1,2).equals(",");
+		return input.length() <= 2*sizeLength+1 && isOneComma(input) && isNum(input.substring(0,commaPos(input))) && isNum(input.substring(commaPos(input)+1,input.length()));
 	}
-	public static boolean isNum(String input) {
+	public boolean isOneComma(String input) {
+		boolean comma = false;
+		for (int i=0; i<input.length(); i++) {
+			if (input.substring(i,i+1).equals(",")){
+				if (!comma){
+					comma = true;
+				}
+				else {
+					return false;
+				}
+			}
+		}
+		return comma;
+	}
+	public int commaPos(String input) {
+		for (int i=0; i < input.length(); i++) {
+			if (input.substring(i,i+1).equals(",")) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public boolean isNum(String input) {
 	    boolean isNum = true;
 	    try {
 
@@ -164,10 +186,6 @@ public class JasonBackEnd implements DavidSupport{
 	    }
 	    return isNum;
 	}
-	public boolean isCheatCode(String str) {
-		return str.equals("getgood");
-	}
 	
 }
-
 
