2ba621c65a0e9667805b2b5c30bd9581ad95ae40
Coby <Coby@Coby-PC.T-mobile.com>
2017-11-26 17:06:22 -0500

    Finished backend.

--- NightOfTheMuseum/src/cobyZhehao/CobyBackEnd.java
+++ NightOfTheMuseum/src/cobyZhehao/CobyBackEnd.java
@@ -10,9 +10,11 @@ public class CobyBackEnd implements ZhehaoSupport{
 	private ZhehaoFrontEnd frontend;
 	private int[] randNums;
 	private String[][] answer= {{"1","2","3","4"},{"5","6","7","8"},{"9","10","11","12"},{"13","14","15"," "}};
-	//private String[][] starting = {{"1","5","10","9"},{"15"," ","4","14"},{"12","2","8","13"},{"11","7","3","6"}};
-	private String[][] starting = {{"1","2","3","4"},{"5","6","7","8"},{"9","10","11","12"},{"13","14"," ","15"}};
-	private int[] blankSpot = {3,2};
+	private String[][] starting = {{"1","5","10","9"},{"15"," ","4","14"},{"12","2","8","13"},{"11","7","3","6"}};
+	private int[] blankSpot = {1,1};
+	//Underneath is to test
+	//private String[][] starting = {{"1","2","3","4"},{"5","6","7","8"},{"9","10","11","12"},{"13","14"," ","15"}};
+	//private int[] blankSpot = {3,2};
 	public ZhehaoCobyPlot[][] plots;
 	private String keyword = "math";
 	public static Scanner in;
