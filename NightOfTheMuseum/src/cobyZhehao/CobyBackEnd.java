3244b6b7790fa101126b2964d7b1884422c9531b
Coby <Coby@Coby-PC.T-mobile.com>
2017-11-27 00:19:19 -0500

    a

--- NightOfTheMuseum/src/cobyZhehao/CobyBackEnd.java
+++ NightOfTheMuseum/src/cobyZhehao/CobyBackEnd.java
@@ -10,11 +10,10 @@ public class CobyBackEnd implements ZhehaoSupport{
 	private ZhehaoFrontEnd frontend;
 	private int[] randNums;
 	private String[][] answer= {{"1","2","3","4"},{"5","6","7","8"},{"9","10","11","12"},{"13","14","15"," "}};
-	//private String[][] starting = {{"1","5","10","9"},{"15"," ","4","14"},{"12","2","8","13"},{"11","7","3","6"}};
-	private String[][] starting = {{"1","2","3","4"},{"5","6","7","8"},{"9","10","11","12"},{"13","14"," ","15"}};
-	private int[] blankSpot = {3,2};
+	private String[][] starting = {{"1","5","10","9"},{"15"," ","4","14"},{"12","2","8","13"},{"11","7","3","6"}};
+	//private String[][] starting = {{"1","2","3","4"},{"5","6","7","8"},{"9","10","11","12"},{"13","14"," ","15"}};
+	private int[] blankSpot = {1,1};
 	public ZhehaoCobyPlot[][] plots;
-	private String keyword = "math";
 	public static Scanner in;
 	
 	public CobyBackEnd(ZhehaoFrontEnd frontend) {
@@ -135,12 +134,5 @@ public class CobyBackEnd implements ZhehaoSupport{
 		p.setContentsNull();
 		blankSpot[0] = p.getRow();
 		blankSpot[1] = p.getCol();
-	}
-	
-	public boolean keyWordUsed() {
-		if(frontend.in.nextLine().equals(keyword)) {
-			return true;
-		}
-		return false;
 	}
 }
