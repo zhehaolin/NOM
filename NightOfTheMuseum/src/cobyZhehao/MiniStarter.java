326a4b4c42c56cd6accdc4859511e94cdebdb434
zhehaolin <31856966+zhehaolin@users.noreply.github.com>
2017-11-26 23:52:33 -0500

    Update MiniStarter.java

--- NightOfTheMuseum/src/cobyZhehao/MiniStarter.java
+++ NightOfTheMuseum/src/cobyZhehao/MiniStarter.java
@@ -3,12 +3,24 @@ package cobyZhehao;
 import caveExplorer.NPC;
 
 public class MiniStarter extends NPC {
+	private String activeDescription;
+	private String inactiveDescription;
 
-	public MiniStarter() {
-		// TODO Auto-generated constructor stub
+	public MiniStarter(String activeDescription, String inactiveDescription) {
+		this.activeDescription = activeDescription;
+		this.inactiveDescription = inactiveDescription;
 	}
 	public void interact() {
 		ZhehaoFrontEnd.main(null);
 	}
+	public String getInactiveDescription() {
+		return inactiveDescription;
+	}
+
+	public String getActiveDescription() {
+		return activeDescription;
+	}
+
+
 
 }
